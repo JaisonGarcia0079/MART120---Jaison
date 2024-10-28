3c746495892b47d75c0aba7bfe72303c99c76f4c
JaisonGarcia0079 <jaison.garcia@umconnect.umt.edu>
2024-10-28 16:38:17 -0700

    convert to main

---
+++ Homework 9/sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(209, 120, 31);
+ 
+   fill(107,68,13);
+ ellipse(250,90,200,120)
+  fill(120,31,31);
+   square(80,290,350);red 
+  fill(235,196,141)
+  ellipse(250,330,150,180);tan
+  circle(250,190,250);tan; 
+     triangle(240, 220, 250, 180, 260,220);
+   fill(255,255,255)
+   circle(190,160,40);
+  circle(300,160,40);
+  fill(107,68,13);
+  circle(190,160,20);
+  circle(300,160,20);
+  fill(0,0,0)
+  circle(190,160,8);
+  circle(300,160,8);
+      strokeWeight(3); 
+  line(190, 250, 300, 250);
+
+  push();
+translate(270,1);
+rotate(45);
+fill(107, 68, 13);
+rect(10, 40, 65, 150);
+pop();
+
+  push();
+translate(270, 1); 
+scale(-1, 1);
+rotate(45);
+fill(107, 68, 13);
+rect(10, 20, 65, 150); 
+pop();
+ 
+
+}
